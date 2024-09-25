ad229e8a15362728aacfb9e9a6cb26f27acd4bd1
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-09-25 13:28:53 +0000

    Finish project

--- scripts.js
+++ scripts.js
@@ -1,20 +1,44 @@
 const MAX_SIZE = 480
+const DEFAULT_SIZE = 16;
 
-let grid_size = 100;
-let grid_area = grid_size * grid_size;
-
+const btn = document.querySelector("button");
+const inp = document.querySelector("input");
 const container = document.querySelector(".container")
-for(let i = 0; i < grid_area; i++) {
-    const child = document.createElement("div");
-    child.classList.add("pixel");
-    child.style.minWidth = `${MAX_SIZE / grid_size}px` 
-    child.style.minHeight = `${MAX_SIZE / grid_size}px` 
-    container.appendChild(child);
+
+function set_grid(size) {
+    let arrayOfNewChildren = [];
+    for (let i = 0; i < size * size; i++) {
+        const child = document.createElement("div");
+        child.classList.add("pixel");
+        child.style.minWidth = `${MAX_SIZE / size}px` 
+        child.style.minHeight = `${MAX_SIZE / size}px` 
+        arrayOfNewChildren.push(child);
+    }
+    container.replaceChildren(...arrayOfNewChildren);
 }
 
+function change_size(event) {
+    let new_size = parseInt(inp.value, 10);
+    if(isNaN(new_size)) new_size = DEFAULT_SIZE;
+    console.log(new_size);
+    // Limit new_size between 1 - 100 (inclusive)
+    new_size = Math.min(new_size, 100);
+    new_size = Math.max(new_size, 1);
+    set_grid(new_size);
+    inp.value = "";
+    inp.placeholder = `${new_size}`;
+};
+
+btn.addEventListener("click", change_size);
+inp.addEventListener("keypress", (e) => {
+    if(e.key !== 'Enter') return;
+    change_size();  
+});
 
 container.addEventListener("mouseover", (event) => {
     const target = event.target;
     if(target.className !== "pixel") return;
     target.classList.add("drawn");
 });
+
+set_grid(DEFAULT_SIZE);
