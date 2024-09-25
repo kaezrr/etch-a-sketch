9f1614e25c131f02f2647be1e7ceb200ecf8d9cc
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-09-25 12:16:17 +0000

    Finish main webpage framework

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
+const MAX_SIZE = 480
+
+let grid_size = 100;
+let grid_area = grid_size * grid_size;
+
 const container = document.querySelector(".container")
-for(let i = 0; i < 16 * 16; i++) {
+for(let i = 0; i < grid_area; i++) {
     const child = document.createElement("div");
     child.classList.add("pixel");
-    child.style.minHeight = "20px";
-    child.style.minWidth = "20px";
+    child.style.minWidth = `${MAX_SIZE / grid_size}px` 
+    child.style.minHeight = `${MAX_SIZE / grid_size}px` 
     container.appendChild(child);
 }
+
+
+container.addEventListener("mouseover", (event) => {
+    const target = event.target;
+    if(target.className !== "pixel") return;
+    target.classList.add("drawn");
+});
