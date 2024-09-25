a6e01c7a90ebe90c493256c67edd594a6c6cc9c3
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-09-25 13:56:11 +0000

    Add bonus features, random colors on hover and increasing opacity

--- scripts.js
+++ scripts.js
@@ -5,6 +5,14 @@ const btn = document.querySelector("button");
 const inp = document.querySelector("input");
 const container = document.querySelector(".container")
 
+function get_random_int() {
+    return Math.floor(Math.random() * 255);
+}
+
+function get_random_color() {
+    return `rgb(${get_random_int()}, ${get_random_int()}, ${get_random_int()})`;
+}
+
 function set_grid(size) {
     let arrayOfNewChildren = [];
     for (let i = 0; i < size * size; i++) {
@@ -37,8 +45,16 @@ inp.addEventListener("keypress", (e) => {
 
 container.addEventListener("mouseover", (event) => {
     const target = event.target;
-    if(target.className !== "pixel") return;
-    target.classList.add("drawn");
+    switch(target.className) {
+        case "pixel":
+            target.className = "drawn";
+            target.style.backgroundColor = get_random_color();
+            target.style.opacity = "0.1";
+            return;
+        case "drawn":
+            target.style.opacity = `${Math.min(1, parseFloat(target.style.opacity) + 0.1)}`;
+            return;
+    }
 });
 
 set_grid(DEFAULT_SIZE);
